418661e3b9e9287a5a8e65a031aa7dce6e46603a
ajgarcia09 <ajgarciaramirez@miners.utep.edu>
2016-09-25 17:10:37 -0600

    Created winPrototype project

---
+++ winPrototype/src/winPrototype/WinUserInterface.java
@@ -0,0 +1,11 @@
+package winPrototype;
+
+public class WinUserInterface {
+
+	public static void main(String[] args) {
+		System.out.println("First prototype");
+		System.out.println("Yay!");
+
+	}
+
+}
