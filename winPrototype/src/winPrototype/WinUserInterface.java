7415da1da22301a33d0e653e3a36fda24a179496
ajgarcia09 <AnaJacqueline@WINDOWS-RBUFAVU>
2016-09-25 17:23:10 -0600

    Added print statement

--- winPrototype/src/winPrototype/WinUserInterface.java
+++ winPrototype/src/winPrototype/WinUserInterface.java
@@ -5,6 +5,7 @@ public class WinUserInterface {
 	public static void main(String[] args) {
 		System.out.println("First prototype");
 		System.out.println("Yay!");
+		System.out.println("Let's get to work");
 
 	}
 
