5842ac9e67c98b722d80b63f956f0e277048c86f
ajgarcia09 <AnaJacqueline@WINDOWS-RBUFAVU>
2016-09-25 21:21:18 -0600

    Started setting up the main frame

--- winPrototype/src/winPrototype/WinUserInterface.java
+++ winPrototype/src/winPrototype/WinUserInterface.java
@@ -1,12 +1,13 @@
 package winPrototype;
+import java.awt.*;
+import javax.swing.*;
 
 public class WinUserInterface {
-
 	public static void main(String[] args) {
-		System.out.println("First prototype");
-		System.out.println("Yay!");
-		System.out.println("Let's get to work");
-
+		JFrame windowFrame = new JFrame(); //Class JFrame creates a "block" or "window" 	
+		BorderLayout windowLayout = new BorderLayout(); //
+		windowFrame.setLayout(windowLayout);//assigns the layout to the frame
+		
 	}
 
 }
